6800133e15ed34cc6746ec1f086b87e4a1c3e512
GilbertoCunha <gcacademic@outlook.pt>
2021-10-26 15:10:30 +0100

    start implementing LookAhead Tree building algorithm

--- src/trees/latree.py
+++ src/trees/latree.py
@@ -14,12 +14,16 @@ TreeNode = Union[TreeEvidenceNode, TreeBeliefNode]
 
 
 class LookAheadTree:
+    lookahead: int
     root: TreeEvidenceNode
     action_space: pd.DataFrame 
     evidence_space: pd.DataFrame 
     state_space: pd.DataFrame 
 
-    def __init__(self, ddn: DynamicDecisionNetwork, evidence: dict[Id, int]):
+    def __init__(self, ddn: DynamicDecisionNetwork, evidence: dict[Id, int], lookahead: int):
+        # Save lookahead
+        self.lookahead = lookahead
+
         # Define the spaces of actions, evidences and states
         self.action_space = self.get_node_space(ddn, ActionNode, ddn.get_time())
         self.evidence_space = self.get_node_space(ddn, EvidenceNode, ddn.get_time()+1)
@@ -28,6 +32,9 @@ class LookAheadTree:
         # Build the tree
         self.root = TreeEvidenceNode(evidence, 1)
 
+    def get_lookahead(self) -> int:
+        return self.lookahead
+
     def get_node_space(self, ddn: DynamicDecisionNetwork, node_type: Type(NetworkNode), time: int) -> pd.DataFrame: 
         nodes = ddn.get_nodes_by_type_and_time(node_type, time)
         value_spaces = [ddn.node_map[node].get_value_space() for node in nodes]
@@ -35,8 +42,46 @@ class LookAheadTree:
         node_space = {nodes[i]: node_space[i] for i in range(len(nodes))}
         return pd.DataFrame(node_space)
 
-    def build_tree(self, ddn: DynamicDecisionNetwork, node: TreeNode):
-        # if type(node) is TreeEvidenceNode:
-           # TODO
-        pass
+    def build_tree(self, ddn: DynamicDecisionNetwork, node: TreeNode, n_samples: int, quantum: bool):
+        depth = self.get_depth()
 
+        # Tree is already built
+        if depth >= 2 * self.get_lookahead():
+            pass
+
+        # In case the current node is an evidence node
+        elif type(node) is TreeEvidenceNode:
+            # Create query variables (state variables)
+            query = ddn.get_nodes_by_type_and_time(StateNode, ddn.get_time()+1)
+            depth = node.get_depth()
+
+            # Create a belief node for every possible action
+            for i in range(len(self.action_space)):
+                # Get the action to this belief node
+                action = self.action_space.iloc[[i]].to_dict(orient="list")
+                action = {k: v[0] for k, v in action.items()}
+
+                # Query network for belief (need the previous action)
+                evidence = {**node.get_evidence(), **node.get_prev_action(), **action}
+                belief = ddn.query(query=query, evidence=evidence, n_samples=n_samples, quantum=quantum)
+
+                # Create new belief node
+                belief_node = TreeBeliefNode(belief, action, depth+1)
+                self.build_tree(ddn, belief_node, n_samples, quantum)
+                node.add_child(belief_node)
+
+        # In case the current node is a belief node
+        elif type(node) is TreeBeliefNode:
+            # Previous action and node depth for the evidence node
+            prev_action = node.get_action()
+            depth = node.get_depth()
+
+            # Create query variables (evidence variables and state variables)
+            state_nodes = ddn.get_nodes_by_type_and_time(StateNode, ddn.get_time()+1)
+            query = state_nodes + ddn.get_nodes_by_type_and_time(EvidenceNode, ddn.get_time()+1)
+            evidence_pt = ddn.query(query=query, n_samples=n_samples, quantum=quantum)            
+
+            # Turn JPT into CPT
+            evidence_pt["Prob"] = evidence_pt.groupby(state_nodes)["Prob"].transform(lambda x: x / x.sum()) 
+            
+                    
--- src/trees/nodes.py
+++ src/trees/nodes.py
@@ -9,15 +9,29 @@ Node = Union["TreeEvidenceNode", "TreeBeliefNode"]
 class TreeEvidenceNode:
     depth: int
     evidence: dict[Id, int]
+    prev_action: dict[Id, int]
     prob: float
     utility: float
     children: list[Node]
 
-    def __init__(self, evidence: dict[Id, int], prob: float, depth: int):
-        self.depth = depth
+    def __init__(self, evidence: dict[Id, int], prev_action: dict[Id, int], prob: float, depth: int):
         self.evidence = evidence
+        self.prev_action = prev_action
         self.prob = prob
+        self.depth = depth
         self.children = []
+
+    def get_depth(self) -> int:
+        return self.depth
+
+    def get_evidence(self) -> dict[Id, int]:
+        return self.evidence
+
+    def get_prev_action(self) -> dict[Id, int]:
+        return self.prev_action
+
+    def add_child(self, node: Node):
+        self.children.append(node)
 
 
 class TreeBeliefNode:
@@ -28,7 +42,16 @@ class TreeBeliefNode:
     children: list[Node]
 
     def __init__(self, belief: dict[Union[Id, str], list[int]], action: dict[Id, int], depth: int):
-        self.depth = depth
         self.belief = belief
         self.action = action
+        self.depth = depth
         self.children = []
+
+    def get_depth(self) -> int:
+        return self.depth
+
+    def get_action(self) -> dict[Id, int]:
+        return self.action
+
+    def add_child(self, node: Node):
+        self.children.append(node)
